085ae1464f7f8285df738afa99f236315cf3ac24
Danish <danishmaulahasbi@gmail.com>
2023-05-27 21:03:26 +0700

    feat : change home using bottom nav bar

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:foodie_hub/presentation/search_restaurant.dart';
-import 'package:foodie_hub/provider/restaurant_provider.dart';
-import 'package:foodie_hub/utils/style_manager.dart';
-import 'package:provider/provider.dart';
-
-import '../utils/shimmer.dart';
-import '../widgets/card_restaurant.dart';
+import 'package:foodie_hub/presentation/list_restaurant_page.dart';
+import 'package:foodie_hub/presentation/setting_page.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = '/home-page';
@@ -17,86 +12,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _bottomNavIndex = 0;
+
+  final List<Widget> _listWidget = [ListRestaurantPage(), SettingPage()];
+
+  final List<BottomNavigationBarItem> _bottomNavBarItems = const [
+    BottomNavigationBarItem(
+        icon: Icon(Icons.home), label: 'Home', backgroundColor: Colors.black),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.settings),
+        label: 'Settings',
+        backgroundColor: Colors.black),
+  ];
+
+  void _onBottomNavTapped(int index) {
+    setState(() {
+      _bottomNavIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Welcome to Foodie Hub',
-                        style: getBlackTextStyle(
-                            fontSize: 24, fontWeight: FontWeight.w600),
-                      ),
-                      Text(
-                        'Your one stop for all your food needs.',
-                        style: getBlackTextStyle(),
-                      ),
-                    ],
-                  ),
-                  const Spacer(),
-                  IconButton(
-                      onPressed: () {
-                        Navigator.pushNamed(
-                            context, SearchRestaurant.routeName);
-                      },
-                      icon: const Icon(
-                        Icons.search,
-                        color: Colors.black,
-                      ))
-                ],
-              ),
-              const SizedBox(height: 24),
-              Expanded(
-                child: _buildList(context),
-              )
-            ],
-          ),
-        ),
+      body: _listWidget[_bottomNavIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        currentIndex: _bottomNavIndex,
+        items: _bottomNavBarItems,
+        onTap: _onBottomNavTapped,
       ),
-    );
-  }
-
-  Widget _buildList(BuildContext context) {
-    return Consumer<RestaurantProvider>(builder: (context, state, _) {
-      if (state.state == ResultState.loading) {
-        return const Center(child: ShimmerContainer());
-      } else if (state.state == ResultState.hasData) {
-        return ListView.builder(
-            physics: const BouncingScrollPhysics(),
-            shrinkWrap: true,
-            itemCount: state.result.restaurants.length,
-            itemBuilder: (context, index) {
-              var restaurant = state.result.restaurants[index];
-              return CardRestaurant(
-                restaurant: restaurant,
-              );
-            },
-          );
-        } else if (state.state == ResultState.noData) {
-          return Center(
-            child: Material(
-              child: Text(state.message),
-            ),
-          );
-        } else if (state.state == ResultState.error) {
-          return Center(
-            child: Material(
-              child: Text(state.message),
-            ),
-          );
-        } else {
-          return const Center(child: Text(''));
-        }
-      },
     );
   }
 }
---
+++ lib/presentation/list_restaurant_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:foodie_hub/presentation/search_restaurant.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/restaurant_provider.dart';
+import '../utils/shimmer.dart';
+import '../utils/style_manager.dart';
+import '../widgets/card_restaurant.dart';
+
+class ListRestaurantPage extends StatelessWidget {
+  const ListRestaurantPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Welcome to Foodie Hub',
+                        style: getBlackTextStyle(
+                            fontSize: 24, fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        'Your one stop for all your food needs.',
+                        style: getBlackTextStyle(),
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  IconButton(
+                      onPressed: () {
+                        Navigator.pushNamed(
+                            context, SearchRestaurant.routeName);
+                      },
+                      icon: const Icon(
+                        Icons.search,
+                        color: Colors.black,
+                      ))
+                ],
+              ),
+              const SizedBox(height: 24),
+              Expanded(
+                child: _buildList(context),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildList(BuildContext context) {
+    return Consumer<RestaurantProvider>(
+      builder: (context, state, _) {
+        if (state.state == ResultState.loading) {
+          return const Center(child: ShimmerContainer());
+        } else if (state.state == ResultState.hasData) {
+          return ListView.builder(
+            physics: const BouncingScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: state.result.restaurants.length,
+            itemBuilder: (context, index) {
+              var restaurant = state.result.restaurants[index];
+              return CardRestaurant(
+                restaurant: restaurant,
+              );
+            },
+          );
+        } else if (state.state == ResultState.noData) {
+          return Center(
+            child: Material(
+              child: Text(state.message),
+            ),
+          );
+        } else if (state.state == ResultState.error) {
+          return Center(
+            child: Material(
+              child: Text(state.message),
+            ),
+          );
+        } else {
+          return const Center(child: Text(''));
+        }
+      },
+    );
+  }
+}
